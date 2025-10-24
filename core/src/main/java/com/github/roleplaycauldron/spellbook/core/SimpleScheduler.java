0c019a9a909dabf23508bbecfe450dbab26e8a64
Velyn-N <nils01.gaming@gmail.com>
2025-10-24 16:35:04 +0200

    Move to correct Package + JavaDoc

--- core/src/main/java/com/github/roleplaycauldron/spellbook/core/SimpleScheduler.java
+++ core/src/main/java/com/github/roleplaycauldron/spellbook/core/SimpleScheduler.java
@@ -1,13 +1,21 @@
-package com.github.roleplaycauldron.minecraft.lib.core;
+package com.github.roleplaycauldron.spellbook.core;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
 
+/**
+ * An easy-to-use Scheduler that makes running Tasks asynchronously a breeze!
+ */
 public class SimpleScheduler {
     private final JavaPlugin plugin;
 
+    /**
+     * Create a new {@link SimpleScheduler}
+     *
+     * @param plugin the Main class of the Plugin using this Scheduler
+     */
     public SimpleScheduler(JavaPlugin plugin) {
         this.plugin = plugin;
     }
@@ -15,6 +23,10 @@ public class SimpleScheduler {
     /**
      * Runs the given callable asynchronously and returns a CompletableFuture
      * that will be completed with the result or exceptionally if an error occurs.
+     *
+     * @param callable the Task to asynchronously execute
+     * @return A {@link CompletableFuture} of the Tasks return value
+     * @param <T> the Tasks return values type
      */
     public <T> CompletableFuture<T> runTaskAsync(Callable<T> callable) {
         CompletableFuture<T> future = new CompletableFuture<>();
@@ -32,6 +44,9 @@ public class SimpleScheduler {
     /**
      * Runs the given runnable asynchronously and returns a CompletableFuture
      * that will be completed when the task finishes.
+     *
+     * @param runnable the Task to asynchronously execute
+     * @return A {@link CompletableFuture} without a return value
      */
     public CompletableFuture<Void> runTaskAsync(Runnable runnable) {
         CompletableFuture<Void> future = new CompletableFuture<>();
@@ -46,4 +61,3 @@ public class SimpleScheduler {
         return future;
     }
 }
-
