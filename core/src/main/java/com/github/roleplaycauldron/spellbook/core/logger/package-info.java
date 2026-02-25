46e7d12758657e9a6f3266f6044ddc41945b44dc
Lorias-Jak <jannik.k11@gmail.com>
2026-02-25 13:46:27 +0100

    Added package-info

---
+++ core/src/main/java/com/github/roleplaycauldron/spellbook/core/logger/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Provides interfaces and classes for logging with enhanced formatting capabilities and debug mode support.
+ * This package includes a logger interface and its implementation,
+ * allowing for standardized logging across the plugin with flexible debug mode control.
+ */
+package com.github.roleplaycauldron.spellbook.core.logger;
---
+++ database/src/main/java/com/github/roleplaycauldron/spellbook/database/package-info.java
@@ -0,0 +1,1 @@
+package com.github.roleplaycauldron.spellbook.database;
