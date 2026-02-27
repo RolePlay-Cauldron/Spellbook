7716ab5225bda83328744b483579297d6508b76f
Lorias-Jak <jannik.k11@gmail.com>
2026-02-27 13:32:57 +0100

    Added a FileBackupService to back up files and zip them with automated backup retention

---
+++ core/src/main/java/com/github/roleplaycauldron/spellbook/core/file/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Provides interfaces and classes for file operations.
+ * This package currently provides utilities for file backups.
+ */
+package com.github.roleplaycauldron.spellbook.core.file;
