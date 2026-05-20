5c0c0be4cedaeab28aeed3c891b532e1ba4d0138
Lorias-Aegorin <Jannik.k11@gmail.com>
2026-05-20 13:30:35 +0200

    Added `removeUnordered` method to `PointBuffer` for O(1) removals and corresponding unit tests. Also refined the javadocs

--- effect/src/test/java/com/github/roleplaycauldron/spellbook/effect/PointBufferTest.java
+++ effect/src/test/java/com/github/roleplaycauldron/spellbook/effect/PointBufferTest.java
@@ -51,6 +51,38 @@ class PointBufferTest {
     }
 
     @Test
+    void testRemoveUnorderedMovesLastPointIntoRemovedSlot() {
+        PointBuffer buffer = new PointBuffer();
+        buffer.add(1, 2, 3);
+        buffer.add(4, 5, 6);
+        buffer.add(7, 8, 9);
+
+        buffer.removeUnordered(0);
+
+        assertEquals(2, buffer.size());
+        assertEquals(7f, buffer.x(0), 1e-6f);
+        assertEquals(8f, buffer.y(0), 1e-6f);
+        assertEquals(9f, buffer.z(0), 1e-6f);
+        assertEquals(4f, buffer.x(1), 1e-6f);
+        assertEquals(5f, buffer.y(1), 1e-6f);
+        assertEquals(6f, buffer.z(1), 1e-6f);
+    }
+
+    @Test
+    void testRemoveUnorderedCanRemoveLastPoint() {
+        PointBuffer buffer = new PointBuffer();
+        buffer.add(1, 2, 3);
+        buffer.add(4, 5, 6);
+
+        buffer.removeUnordered(1);
+
+        assertEquals(1, buffer.size());
+        assertEquals(1f, buffer.x(0), 1e-6f);
+        assertEquals(2f, buffer.y(0), 1e-6f);
+        assertEquals(3f, buffer.z(0), 1e-6f);
+    }
+
+    @Test
     void testVectorSnapshotIsDefensive() {
         PointBuffer buffer = new PointBuffer();
         buffer.add(1, 2, 3);
